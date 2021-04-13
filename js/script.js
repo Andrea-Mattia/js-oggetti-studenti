fa4f6773b12f85d8d408ebd6b1c0b93d915185ab
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-04-13 17:30:17 +0200

    add ex.1 solution

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log('Works!');
+/**
+ * Descrizione
+ * Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età. Stampare a
+ * schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+ * Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e
+ * cognome.
+ * Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo
+ * nell’ordine: nome, cognome e età. Una volta creato il nuovo oggetto inserirlo nell'array creato al punto 2
+ * e mostrare alla fine il contenuto dell'array.
+ */
+
+/*****************
+ * ESERCIZIO 1
+ *****************/
+
+// Ref
+var student = {
+    firstName: 'Pippo',
+    lastName: 'Franco',
+    age: 80,
+}
+
+// Iterazione per stampare le proprietà dell'oggetto
+for (key in student) {
+    console.log(key + ':', student[key]);
+}
