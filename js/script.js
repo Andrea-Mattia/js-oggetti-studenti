f34fd9efc736c717a385e1ef16a7b46e5121c7f6
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-04-13 19:17:36 +0200

    added html and css and inserted result in DOM

--- js/script.js
+++ js/script.js
@@ -13,71 +13,94 @@
  * ESERCIZIO 1
  *****************/
 
-// Ref
-var student = {
-    firstName: 'Pippo',
-    lastName: 'Franco',
-    age: 80,
-}
+$(document).ready(function () {
+    
+    // Refs
+    var btn1 = $('.btn-1');
 
-console.log('---- ESERCIZIO 1 ----');
+    btn1.click(function() { 
+        // Ref
+        var student = {
+            firstName: 'Pippo',
+            lastName: 'Franco',
+            age: 80,
+        }
 
-// Iterazione per stampare le proprietà dell'oggetto
-for (key in student) {
-    console.log(key + ':', student[key]);
-}
+        // Iterazione per stampare le proprietà dell'oggetto
+        for (key in student) {
+            var result1 = $('.ex-1 .result');
+            result1.append(key + ': ' + student[key] + '<br>');
+        }
+    });
 
-/*****************
- * ESERCIZIO 2
- *****************/
+    /*****************
+     * ESERCIZIO 2
+     *****************/
 
-// Ref
-var students = [
-    {
-        nome: 'Carcarlo',
-        cognome: 'Pravettoni',
-        eta: 66,
-    },
-    {
-        nome: 'Piergiacomo',
-        cognome: 'Rosetta',
-        eta: 21,
-    },
-    {
-        nome: 'Gastani',
-        cognome: 'Frinzi',
-        eta: 16,
-    },
-    {
-        nome: 'Filippo',
-        cognome: 'Giallini',
-        eta: 35,
-    },
-];
+    // Refs
+    var btn2 = $('.btn-2');
 
-console.log('---- ESERCIZIO 2 ----');
+    // Ref
+    var students = [
+        {
+            nome: 'Carcarlo',
+            cognome: 'Pravettoni',
+            eta: 66,
+        },
+        {
+            nome: 'Piergiacomo',
+            cognome: 'Rosetta',
+            eta: 21,
+        },
+        {
+            nome: 'Gastani',
+            cognome: 'Frinzi',
+            eta: 16,
+        },
+        {
+            nome: 'Filippo',
+            cognome: 'Giallini',
+            eta: 35,
+        },
+    ];
 
-// Iterazione per stampare nome e cognome degli studenti
-for (var i = 0; i < students.length; i++) {
-    var alumn = students[i];
-    console.log('Nome: ', alumn.nome);
-    console.log('Cognome: ', alumn.cognome);
-}
+    btn2.click(function() {
 
-/*****************
- * ESERCIZIO 3
- *****************/
+        // Iterazione per stampare nome e cognome degli studenti
+        for (var i = 0; i < students.length; i++) {
 
-// Refs
-var newStudent = {};
+            var result2 = $('.ex-2 .result');
+            var alumn = students[i];
 
-// Inserimento dei dati del nuovo studente da parte dell'utente
-newStudent.nome = prompt('Inserisci il nome dello studente').trim();
-newStudent.cognome = prompt('Inserisci il cognome dello studente').trim();
-newStudent.eta = prompt('Inserisci l\' età dello studente').trim();
+            result2.append('Nome: ' + alumn.nome + '<br>' + 'Cognome: ' + alumn.cognome + '<br><br>');
+        }
+    });
 
-// Inserimento del nuovo studente nell'array students
-students.push(newStudent);
+    /*****************
+     * ESERCIZIO 3
+     *****************/
 
-// Stampo il contenuto del'array students
-console.log(students);
+    // Refs
+    var btn3 = $('.btn-3');
+
+    btn3.click(function() {
+        // Refs
+        var newStudent = {};
+
+        // Inserimento dei dati del nuovo studente da parte dell'utente
+        newStudent.nome = prompt('Inserisci il nome dello studente').trim();
+        newStudent.cognome = prompt('Inserisci il cognome dello studente').trim();
+        newStudent.eta = parseInt( prompt('Inserisci l\' età dello studente').trim() );
+
+        // Inserimento del nuovo studente nell'array students
+        students.push(newStudent);
+
+        // Stampo il contenuto del'array students
+        $('.ex-3 .result').text('Guarda nella console!!');
+
+        console.log('Students: ', students);
+        console.table(students);
+    });
+
+    // End doc ready
+});
